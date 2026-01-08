f79ad6349f905ba1a3f60884ac1ac95920c6f5fa
Athul Manoharan Nair(UST <304501@ust.com>
2026-01-08 17:19:33 +0530

    1st question pushed

---
+++ Search1.java
@@ -0,0 +1,37 @@
+class Search1 {
+    public int search(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+
+        while (left <= right) {
+            int mid = (left + right) / 2;
+
+            if (nums[mid] == target) {
+                return mid;
+            }
+            else if (nums[mid] >= nums[left]) {
+                if (nums[left] <= target && target <= nums[mid]) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+            } else {
+                if (nums[mid] <= target && target <= nums[right]) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
+            }
+        }
+
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        Search1 searcher = new Search1();
+        int[] nums = {4,5,6,7,0,1,2};
+        int target = 0;
+        int result = searcher.search(nums, target);
+        System.out.println("Index of target " + target + ": " + result);
+    }
+}
